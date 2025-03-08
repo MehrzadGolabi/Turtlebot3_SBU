96edb08c494ca2c4ae8ccf64ebff47205783b7cc
thanotos <thanotos@thanotos-G15-kubuntu>
2025-03-09 00:43:51 +0330

    lidar added

---
+++ turtlebot_control_simulation/turtle_lidar.m
@@ -0,0 +1,9 @@
+%% lidarTurtlebot3.m
+
+
+setenv('ROS_DOMAIN_ID','30');
+scan_node = ros2node("/plot_lidar");
+ScanSub = ros2subscriber(scan_node,"/scan","sensor_msgs/LaserScan");
+scanMsg = receive(ScanSub);
+
+rosPlot(scanMsg);
